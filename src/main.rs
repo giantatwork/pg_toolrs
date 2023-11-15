d4cd648c8c6472ea53b663aa6cdfa640d5def975
Giant <info@giantatwork.nl>
2023-11-15 11:32:32 +0100

    work in progress

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
 // use std::env;
 use std::{
     io::{Read, Write},
-    process::{Command, Stdio},
+    process::{Command, ExitStatus, Stdio},
 };
 
 use indicatif::{ProgressBar, ProgressStyle};
 
-fn dump(
-    db_name: String,
-    db_user: String,
-    dump_file: Option<String>,
-    docker_container: Option<String>,
-) {
+#[derive(Debug)]
+enum DropDBError {
+    CommandFailed(ExitStatus),
+    IoError(std::io::Error),
+}
+
+fn drop_db(
+    db_name: &str,
+    db_user: &str,
+    docker_container: Option<&str>,
+) -> Result<(), DropDBError> {
+    let mut command = Command::new("dropdb");
+
+    match docker_container {
+        Some(container) => {
+            command.arg("docker").arg("exec").arg("-i").arg(container);
+        }
+        _ => {}
+    }
+    command.arg(db_name).arg("-U").arg(db_user);
+
+    let result = command.output();
+    match result {
+        Ok(output) => {
+            if output.status.success() {
+                Ok(())
+            } else {
+                Err(DropDBError::CommandFailed(output.status))
+            }
+        }
+        Err(err) => Err(DropDBError::IoError(err)),
+    }
+}
+
+fn dump(db_name: &str, db_user: &str, dump_file: Option<&str>, docker_container: Option<&str>) {
     let mut command = Command::new("pg_dump");
 
     match docker_container {
@@ -21,7 +50,7 @@ fn dump(
         _ => {}
     }
 
-    let dump_path = dump_file.unwrap_or("db.dump".to_string());
+    let dump_path = dump_file.unwrap_or("db.dump");
 
     command
         .arg("-U")
@@ -33,12 +62,10 @@ fn dump(
         .stdout(Stdio::piped());
 
     let mut res = command.spawn().expect("Failed to start command");
-
     let mut stdout = res.stdout.take().expect("Failed to capture stdout");
-
     let mut buffer = [0; 4096];
-
     let mut file = std::fs::File::create(dump_path).expect("Failed to create file");
+
     let pb = ProgressBar::new_spinner();
     pb.set_style(
         ProgressStyle::default_spinner()
@@ -69,5 +96,5 @@ fn main() {
     // let arg1 = &args[1];
     // println!("First argument: {}", arg1);
 
-    dump("doorsight".to_string(), "adjan".to_string(), None, None);
+    dump("doorsight", "adjan", None, None);
 }
